73c99daef0b4c770b8704a93992e6f56930f2755
Pierre-Alexandre St-Jean <pa@stjean.me>
2019-04-25 08:56:24 -0400

    Try with githubworkspace

--- gradle_setup/entrypoint.sh
+++ gradle_setup/entrypoint.sh
@@ -2,10 +2,8 @@
 
 set -x -e +o pipefail
 
-mkdir -p $HOME/.gradle
-
 echo "
 org.gradle.parallel=true
 org.gradle.daemon=false
 $MAVEN_REPOSITORY_USER_KEY=${MAVEN_REPOSITORY_USER}
-$MAVEN_REPOSITORY_PASSWORD_KEY=${MAVEN_REPOSITORY_PASSWORD}" >> $HOME/.gradle/gradle.properties
+$MAVEN_REPOSITORY_PASSWORD_KEY=${MAVEN_REPOSITORY_PASSWORD}" >> $GITHUB_WORKSPACE/gradle.properties
