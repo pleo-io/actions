cf9058368ddaad0281083d1f21f9fd2a27f03ea3
Pierre-Alexandre St-Jean <pa@stjean.me>
2019-10-04 09:04:34 -0400

    Actions builder

--- comment_pr_deploy_link/entrypoint.sh
+++ comment_pr_deploy_link/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-SANITIZED_BRANCH_NAME=$(echo $BRANCH_NAME | tr ".\\\/+" "-")
+SANITIZED_BRANCH_NAME=$(echo "$BRANCH_NAME" | tr ".\\\/+" "-")
 COMMENT="The app for PR is available at https://$SANITIZED_BRANCH_NAME.backoffice.staging.pleo.io"
 
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
