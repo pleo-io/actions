c862b92ac5f9b3428f1ff279185f993efb120fea
Maciek Pekala <m@peka.la>
2019-10-28 17:49:06 +0100

    Use octokit/app for authenticating with github app

--- github-deploy/src/main.ts
+++ github-deploy/src/main.ts
@@ -1,59 +1,44 @@
-import * as core from '@actions/core'
-import * as jwt from 'jsonwebtoken'
-import Octokit from '@octokit/rest'
+import * as core from "@actions/core";
+import Octokit from "@octokit/rest";
+import { App } from "@octokit/app";
 
-const previews = [
-  'ant-man-preview',
-  'machine-man-preview'
-]
+const getAuthenticatedOctokit = () => {
+  const githubAppID = +core.getInput("github_app_id");
+  const githubAppInstallationID = +core.getInput("github_app_installation_id");
+  const githubAppPrivateKey = core.getInput("github_app_private_key");
+
+  const app = new App({ id: githubAppID, privateKey: githubAppPrivateKey });
+  return new Octokit({
+    async auth() {
+      const installationAccessToken = await app.getInstallationAccessToken({
+        installationId: githubAppInstallationID
+      });
+      return `token ${installationAccessToken}`;
+    }
+  });
+};
 
 async function run() {
-  try {
-    const githubAppID = core.getInput('github_app_id')
-    const githubAppInstallationID = core.getInput('github_app_installation_id')
-    const githubAppPrivateKey = core.getInput('github_app_private_key')
+  const octokit = getAuthenticatedOctokit();
+  const ref = core.getInput("deploy_ref") || process.env["GITHUB_REF"] || "";
+  const environment = core.getInput("deploy_environment");
+  const ownerRepo =
+    core.getInput("deploy_repository") ||
+    process.env["GITHUB_REPOSITORY"] ||
+    "";
+  const [owner, repo] = ownerRepo.split("/");
 
-    const ownerRepo = core.getInput('deploy_repository') || process.env['GITHUB_REPOSITORY'] || ''
-    const ref = core.getInput('deploy_ref') || process.env['GITHUB_REF'] || ''
-    const env = core.getInput('deploy_environment')
+  console.log(
+    `Triggering Deploy event on '${owner}/${repo}' @ref:'${ref}' in env '${environment}'`
+  );
 
-    const now = Math.floor(Date.now() /1000)
-    var appToken = jwt.sign({
-        iat: now,
-        exp:  now + (10 * 60),
-        iss: githubAppID
-      }, githubAppPrivateKey, { algorithm: 'RS256'})
-
-    const octokitForApp = new Octokit({
-      auth: appToken,
-      previews
-    })
-
-    const token = await octokitForApp.apps.createInstallationToken({
-      installation_id: Number.parseInt(githubAppInstallationID)
-    })
-    
-    const octokitForDeployment = new Octokit({
-      auth: token.data.token,
-      previews
-    })
-
-    const [owner,repo] = ownerRepo.split('/')
-
-    console.log(`Triggering Deploy event on '${owner}/${repo}' @ref:'${ref}' in env '${env}'`)
-
-    const createDeploymentOptions: Octokit.ReposCreateDeploymentParams = {
-      owner: owner,
-      repo: repo,
-      ref: ref,
-      required_contexts: [],
-      environment: env
-    }
-
-    await octokitForDeployment.repos.createDeployment(createDeploymentOptions)
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  await octokit.repos.createDeployment({
+    owner,
+    repo,
+    ref,
+    environment,
+    required_contexts: []
+  });
 }
-  
-run();
+
+run().catch(error => core.setFailed(error.message));
