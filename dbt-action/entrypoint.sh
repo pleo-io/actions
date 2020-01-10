f399713fdf71eb386c21c11c81686c0c52e54608
Luca Florio <luca@pleo.io>
2020-01-10 11:38:07 +0100

    Restore dbt command

--- dbt-action/entrypoint.sh
+++ dbt-action/entrypoint.sh
@@ -16,6 +16,4 @@ printf '%b\n' "$(cat $dbt_profiles_dir/profiles.yml)"
 
 printf "Running dbt project in folder %s with command %s on target %s\n" "$dbt_project_dir" "$dbt_command" "$dbt_target"
 
-# dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
-dbt compile -h
-exit 1
+dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
