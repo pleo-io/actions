7f0a7ee182c64f10c5e1ffac39eb2b699ffb60fb
Luca Florio <luca@pleo.io>
2020-01-10 10:00:02 +0100

    Add logging

--- dbt-action/entrypoint.sh
+++ dbt-action/entrypoint.sh
@@ -11,4 +11,9 @@ dbt_project_dir=$2
 dbt_command=$3
 dbt_target=$4
 
+printf "Using profiles from dir %s:\n" "$dbt_profiles_dir"
+printf '%b\n' "$(cat $dbt_profiles_dir/profiles.yml)"
+
+printf "Running dbt project in folder %s with command %s on target %s\n" "$dbt_project_dir" "$dbt_command" "$dbt_target"
+
 dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
