fe0a7467892ca6e58451e719ff4014e0cf6cc8f5
Luca Florio <luca@pleo.io>
2020-01-10 11:51:57 +0100

    Simplify inputs
    The action can be simplified requiring the user to supply just one arguments.
    The other inputs can be integrated as parameters of the argument itself, so
    it is not required to explicitly pass them as separate inputs.

--- dbt-action/README.md
+++ dbt-action/README.md
@@ -4,27 +4,12 @@ This action executes `dbt` commands. For information about `dbt`, please refer t
 
 ## Inputs
 
-### `dbt-profiles-dir`
-
-**Required** Path to folder containing the `profiles.yml` file.
-
-### `dbt-project-dir`
-
-**Required** Path to the `dbt` project folder.
-
 ### `dbt-command`
 
 **Required** `dbt` command to execute.
-
-### `dbt-target`
-
-**Required** Target profile for `dbt` command.
 
 ## Example usage
 
 uses: pleo-io/actions/dbt-action@master
 with:
-  dbt-profiles-dir: './profiles'
-  dbt-project-dir: './project'
   dbt-command: 'compile'
-  dbt-target: 'dev'
--- dbt-action/entrypoint.sh
+++ dbt-action/entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ "$#" -ne 4 ]; then
-    printf "Illegal number of parameters\n"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
     exit 1
 fi
 
-dbt_profiles_dir=$1
-dbt_project_dir=$2
-dbt_command=$3
-dbt_target=$4
+dbt_command=$1
 
 printf "Using profiles from dir %s:\n" "$dbt_profiles_dir"
 printf '%b\n' "$(cat $dbt_profiles_dir/profiles.yml)"
 
-printf "Running dbt project in folder %s with command %s on target %s\n" "$dbt_project_dir" "$dbt_command" "$dbt_target"
+printf "Running dbt with command %s\n" "$dbt_command"
 
-dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
+dbt $dbt_command
