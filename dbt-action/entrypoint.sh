00253840dbde9ab4760ddc106e38286e41721917
Luca Florio <luca@pleo.io>
2020-01-10 11:17:24 +0100

    Make dbt run as root

--- dbt-action/entrypoint.sh
+++ dbt-action/entrypoint.sh
@@ -11,12 +11,9 @@ dbt_project_dir=$2
 dbt_command=$3
 dbt_target=$4
 
-chown -R appuser:appgroup $dbt_profiles_dir
-chown -R appuser:appgroup $dbt_project_dir
-
 printf "Using profiles from dir %s:\n" "$dbt_profiles_dir"
 printf '%b\n' "$(cat $dbt_profiles_dir/profiles.yml)"
 
 printf "Running dbt project in folder %s with command %s on target %s\n" "$dbt_project_dir" "$dbt_command" "$dbt_target"
 
-dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
+sudo dbt $dbt_command --profiles-dir $dbt_profiles_dir --project-dir $dbt_project_dir --target $dbt_target
