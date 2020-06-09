ddcba3b119512bc2d181725f6b02479a21e6f657
Igor Dolzhenkov <32882870+igorDolzh@users.noreply.github.com>
2020-06-09 18:20:12 +0300

    Github action to create lokalise task (#21)

---
+++ lokalise-create-task/README.md
@@ -0,0 +1,54 @@
+# lokalise-create-task
+
+Lets you create a task for localise
+
+## How to use
+
+```yaml
+name: lokalise-create-task
+
+on:
+  push:
+    # Only run workflow for pushes to specific branches
+    branches:
+      - master
+
+jobs:
+  upload:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: pleo-io/actions/lokalise-file-push
+        with:
+          # Api token for the Lokalise account
+          # with read/write access to the project
+          api-token: ${{ secrets.LOCALIZE_TOKEN }}
+
+          # ID of the project to sync
+          project-id: project-id
+
+          # Tags
+          tags: '["tagName","tagName1"]'
+
+          # Name of team from localise
+          team-name: Pleo team
+
+          # List of email for task assignee
+          assignee-email-list: '{
+            "en": ["igor@pleo.io", "sam@pleo.io"],
+            "fr": ["igor@pleo.io", "sam@pleo.io"],
+            "de": ["igor@pleo.io", "sam@pleo.io"],
+            "es": ["igor@pleo.io", "sam@pleo.io"],
+            "sv": ["igor@pleo.io", "sam@pleo.io"],
+            "da": ["igor@pleo.io", "sam@pleo.io"]
+          }'
+
+          # Task title
+          task-title: Hello, it is a task title
+
+          # Task description
+          task-description: Hello, it is a task description
+
+          # Task options for https://app.lokalise.com/api2docs/curl/#transition-create-a-task-post
+          task-options: '{"auto_close_task": true, "auto_close_languages": true}'
+```
---
+++ lokalise-create-task/src/main.ts
@@ -0,0 +1,120 @@
+import { LokaliseApi } from "@lokalise/node-api";
+import * as ghCore from "@actions/core";
+
+const apiKey = ghCore.getInput("api-token");
+const projectId = ghCore.getInput("project-id");
+const keyTags = ghCore.getInput("tags");
+const teamName = ghCore.getInput("team-name");
+const assigneeEmailList = ghCore.getInput("assignee-email-list");
+const taskTitle = ghCore.getInput("task-title");
+const taskDescription = ghCore.getInput("task-description");
+const taskOptions = ghCore.getInput("task-options");
+
+async function getKeys(lokalise: LokaliseApi) {
+  const keys = await lokalise.keys.list({
+    project_id: projectId,
+    filter_tags: JSON.parse(keyTags).join(),
+  });
+  if (keys && keys.length > 0) {
+    return keys.map((x) => x.key_id);
+  }
+  return null;
+}
+
+async function getAssigneeIdObject(lokalise: LokaliseApi) {
+  const teams = await lokalise.teams.list();
+  const team = teams.find(({ name }) => name === teamName);
+  if (team) {
+    const teamUsers = await lokalise.teamUsers.list({ team_id: team.team_id });
+    const assigneeEmailListParsed = JSON.parse(assigneeEmailList);
+
+    const assigneeIdObject = {} as any;
+    Object.keys(assigneeEmailListParsed).map(function (key: string) {
+      const assigneeEmailListPerLanguage = assigneeEmailListParsed[key];
+
+      const assigneeList = teamUsers.filter(({ email }) =>
+        assigneeEmailListPerLanguage.includes(email)
+      );
+
+      if (assigneeList && assigneeList.length > 0) {
+        assigneeIdObject[key] = assigneeList.map(({ user_id }) => user_id);
+      }
+    });
+    return assigneeIdObject;
+  }
+  return null;
+}
+
+const createTask = async (
+  lokalise: LokaliseApi,
+  language: string,
+  keys: number[],
+  assigneeIdList: number[]
+) => {
+  const options = JSON.parse(taskOptions);
+  await lokalise.tasks.create(
+    {
+      title: taskTitle,
+      description: taskDescription,
+      languages: [
+        {
+          language_iso: language,
+          users: assigneeIdList,
+        },
+      ],
+      keys,
+      ...options,
+    },
+    {
+      project_id: projectId,
+    }
+  );
+};
+
+async function createTasksPerLanguage(
+  lokalise: LokaliseApi,
+  keys: number[],
+  assigneeIdObject: { [key: string]: number[] }
+) {
+  const languageCodes: string[] = Object.keys(assigneeIdObject);
+
+  const starterPromise = Promise.resolve(null);
+
+  await languageCodes.reduce<any>(
+    async (p: Promise<void>, language: string) =>
+      p.then(() =>
+        createTask(lokalise, language, keys, assigneeIdObject[language])
+      ),
+    starterPromise
+  );
+}
+
+const start = async () => {
+  try {
+    const lokalise = new LokaliseApi({
+      apiKey,
+    });
+
+    const keys = await getKeys(lokalise);
+    if (!keys) {
+      throw new Error(`No keys found by '${keyTags}' tags`);
+    }
+
+    const assigneeIdObject = await getAssigneeIdObject(lokalise);
+    if (!assigneeIdObject || Object.keys(assigneeIdObject).length === 0) {
+      throw new Error(
+        `No users inside ${teamName} team with ${assigneeEmailList}`
+      );
+    }
+
+    await createTasksPerLanguage(lokalise, keys, assigneeIdObject);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start()
+  .then(() => console.log("Finished"))
+  .catch((error: Error) =>
+    ghCore.setFailed(error ? error.message : "Unknown error")
+  );
