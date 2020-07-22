6b8de5bd7adf0eafd71ece1d3cdfbc66294ec3c6
Cole Bittel <cole.bittel@gmail.com>
2020-07-22 09:56:28 +0200

    remove comments

--- github-temp-token/src/main.ts
+++ github-temp-token/src/main.ts
@@ -1,23 +1,7 @@
 import * as core from "@actions/core";
 import { App } from "@octokit/app";
 
-// const getToken = (previews?: string[]) => {
-//   const githubAppID = +core.getInput("github_app_id");
-//   const githubAppInstallationID = +core.getInput("github_app_installation_id");
-//   const githubAppPrivateKey = core.getInput("github_app_private_key");
-
-//   const app = new App({ id: githubAppID, privateKey: githubAppPrivateKey });
-//   const installationAccessToken = await app.getInstallationAccessToken({
-//     installationId: githubAppInstallationID
-//   });
-//   return `token ${installationAccessToken}`;
-// };
-
 async function run() {
-  // const token = getToken([
-  //   "ant-man-preview",
-  //   "machine-man-preview"
-  // ]);
   const githubAppID = +core.getInput("github_app_id");
   const githubAppInstallationID = +core.getInput("github_app_installation_id");
   const githubAppPrivateKey = core.getInput("github_app_private_key");
@@ -26,7 +10,6 @@ async function run() {
   const token = await app.getInstallationAccessToken({
     installationId: githubAppInstallationID
   });
-  // return `token ${installationAccessToken}`;
   core.setOutput('github_temp_token', token)
 }
 
