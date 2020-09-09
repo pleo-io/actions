88a663d2b65e73ac1f8dca0728d04e08f604362e
Cole Bittel <cole.bittel@gmail.com>
2020-09-09 13:44:39 +0200

    backend-feature-branch name

---
+++ sanitize-branch-for-dns/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+
+echo GITHUB_HEAD_REF=$GITHUB_HEAD_REF
+echo GITHUB_REF=$GITHUB_REF
+if [ -z "$GITHUB_HEAD_REF" ]
+then
+echo "\$GITHUB_HEAD_REF is empty"
+BN=${GITHUB_REF#"refs/heads/"}
+else
+echo "\$GITHUB_HEAD_REF is NOT empty"
+BN=$GITHUB_HEAD_REF
+fi
+
+
+# Trimming and replacing characters to make URL user friendly
+BN=$(echo "$BN" | tr '/_.' '-')
+BN=$(echo "$BN" | cut -c1-40)
+
+# Abort if can't create branch name, as safety precaution
+if [ -z "$BN" ]
+then
+echo "\$BN is empty"
+exit 1
+fi
+
+echo BN=$BN
+echo ::set-env name=branch_name::${BN}
