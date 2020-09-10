dd921ceb9ab61ad2208d7b169bf7e6cfb4ada83d
Cole Bittel <bittelc@users.noreply.github.com>
2020-09-10 12:07:05 +0200

    backend-feature-branch name (#24)
    * backend-feature-branch name
    
    * output not env
    
    * completion
    
    * add readme

---
+++ sanitize-branch-for-dns/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+
+echo GITHUB_HEAD_REF=$GITHUB_HEAD_REF
+echo GITHUB_REF=$GITHUB_REF
+if [ -z "$GITHUB_HEAD_REF" ]
+then
+echo "\$GITHUB_HEAD_REF is empty"
+BN=${GITHUB_REF#"refs/heads/"}
+else
+echo "\$GITHUB_HEAD_REF is NOT empty"
+BN=$GITHUB_HEAD_REF
+fi
+
+# Trimming and replacing characters to make URL user friendly
+BN=$(echo "$BN" | tr '/_.' '-')
+BN=$(echo "$BN" | cut -c1-40)
+
+# All uppercase to lowercase
+BN=$(echo "$BN" | tr '[A-Z]' '[a-z]')
+echo "lowercased = $BN"
+
+# If ends in dash, remove
+BN=${BN%%-}
+echo "suffix dashes removed = $BN"
+
+# Abort if can't create branch name, as safety precaution
+if [ -z "$BN" ]
+then
+echo "\$BN is empty"
+exit 1
+fi
+
+echo BN=$BN
+echo ::set-output name=branch_name::${BN}
