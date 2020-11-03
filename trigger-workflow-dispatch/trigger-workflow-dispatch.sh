275499f4c59bc696f544a5a7893feb83a566865a
Cole Bittel <cole.bittel@gmail.com>
2020-11-03 17:04:33 +0100

    actually try to run it now

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -1,17 +1,14 @@
-#!/bin/sh
-env
-
-#curl \
-#--fail --silent --show-error \
-#-h "accept: application/vnd.github.v3+json" \
-#-h "authorization: token $workflow_dispatch_deployments" \
-#https://api.github.com/repos/pleo-io//actions/workflows/deploy_primary_dispatch.yaml/dispatches \
-#--data-binary @- << eof
-#{
-#  "ref": "refs/heads/master",
-#  "inputs": {
-#    "sha": "${github_sha}",
-#    "env": "${env}"
-#  }
-#}
-#eof
+curl \
+--fail --silent --show-error \
+-h "accept: application/vnd.github.v3+json" \
+-h "authorization: token $workflow_dispatch_deployments" \
+https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/deploy_primary_dispatch.yaml/dispatches \
+--data-binary @- << EOF
+{
+  "ref": "${WORKFLOW_FILE_REF}",
+  "inputs": {
+    "sha": "${GITHUB_SHA}",
+    "env": "${ENV}"
+  }
+}
+EOF
