0177f9cfd222c03cc671274e1b9059d6caaaeebd
Cole Bittel <cole.bittel@gmail.com>
2020-11-04 11:22:11 +0100

    token needs input

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 env
-echo endpoint = "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW_DISPATCH_FILE_NAME}/dispatches"
-echo ref = "${WORKFLOW_FILE_REF}"
+echo endpoint = "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW_DISPATCH_FILE_NAME}/dispatches"
+echo ref = "${INPUT_WORKFLOW_FILE_REF}"
 echo sha = "${GITHUB_SHA}"
-echo env = "${ENV}"
+echo env = "${INPUT_ENV}"
 
 
 
 curl \
 --fail --silent --show-error \
 -H "Accept: application/vnd.github.v3+json" \
--H "Authorization: token ${GITHUB_DISPATCH_WORKFLOW_TOKEN}" \
+-H "Authorization: token ${INPUT_GITHUB_DISPATCH_WORKFLOW_TOKEN}" \
 "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW_DISPATCH_FILE_NAME}/dispatches" \
 --data-binary @- << EOF
 {
