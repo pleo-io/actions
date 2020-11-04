c9f79bc3a9b86067142f5578297c28e76d58f975
Cole Bittel <cole.bittel@gmail.com>
2020-11-04 11:10:58 +0100

    string thing and spit stuff out

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+env
+echo endpoint = "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW_DISPATCH_FILE_NAME}/dispatches"
+echo ref = "${WORKFLOW_FILE_REF}"
+echo sha = "${GITHUB_SHA}"
+echo env = "${ENV}"
+
+
+
 curl \
 --fail --silent --show-error \
 -H "Accept: application/vnd.github.v3+json" \
 -H "Authorization: token ${GITHUB_DISPATCH_WORKFLOW_TOKEN}" \
-https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW_DISPATCH_FILE_NAME}/dispatches \
+"https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW_DISPATCH_FILE_NAME}/dispatches" \
 --data-binary @- << EOF
 {
   "ref": "${WORKFLOW_FILE_REF}",
