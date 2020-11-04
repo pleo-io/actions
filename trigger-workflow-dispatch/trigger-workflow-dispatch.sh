50cc0e538cce546bd4d9423400824e876da94ee3
Cole Bittel <cole.bittel@gmail.com>
2020-11-04 11:16:06 +0100

    these are all inputs

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -12,13 +12,13 @@ curl \
 --fail --silent --show-error \
 -H "Accept: application/vnd.github.v3+json" \
 -H "Authorization: token ${GITHUB_DISPATCH_WORKFLOW_TOKEN}" \
-"https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW_DISPATCH_FILE_NAME}/dispatches" \
+"https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW_DISPATCH_FILE_NAME}/dispatches" \
 --data-binary @- << EOF
 {
-  "ref": "${WORKFLOW_FILE_REF}",
+  "ref": "${INPUT_WORKFLOW_FILE_REF}",
   "inputs": {
     "sha": "${GITHUB_SHA}",
-    "env": "${ENV}"
+    "env": "${INPUT_ENV}"
   }
 }
 EOF
