2677ac89e88c1bad88cfed799f558f11e05ebcf3
Cole Bittel <cole.bittel@gmail.com>
2020-11-09 12:24:07 +0100

    wrong use of input

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-env
 echo endpoint = "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW_DISPATCH_FILE_NAME}/dispatches"
 echo ref = "${INPUT_WORKFLOW_FILE_REF}"
 echo sha = "${GITHUB_SHA}"
@@ -20,7 +19,7 @@ curl \
   "inputs": {
     "sha": "${GITHUB_SHA}",
     "env": "${INPUT_ENV}",
-    "allow_chained_deployments": "${inputs.ALLOW_CHAINED_DEPLOYMENTS }"
+    "allow_chained_deployments": "${INPUT_ALLOW_CHAINED_DEPLOYMENTS }"
   }
 }
 EOF
