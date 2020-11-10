c6f3c26d2f9790bcb2a685888fe409958474ade1
Cole Bittel <bittelc@users.noreply.github.com>
2020-11-10 11:41:32 +0100

    BIDEN COULDN'T HAVE DONE IT WITHOUT THESE PILLS (#38)
    * Allow specifying workflow chaining
    
    * wrong use of input
    
    * verbose
    
    * maybe the space is bad
    
    * back to smaller output

--- trigger-workflow-dispatch/trigger-workflow-dispatch.sh
+++ trigger-workflow-dispatch/trigger-workflow-dispatch.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-env
 echo endpoint = "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW_DISPATCH_FILE_NAME}/dispatches"
 echo ref = "${INPUT_WORKFLOW_FILE_REF}"
 echo sha = "${GITHUB_SHA}"
 echo env = "${INPUT_ENV}"
-
+echo allow_chained_deployments = "${INPUT_ALLOW_CHAINED_DEPLOYMENTS}"
 
 
 curl \
@@ -18,7 +17,8 @@ curl \
   "ref": "${INPUT_WORKFLOW_FILE_REF}",
   "inputs": {
     "sha": "${GITHUB_SHA}",
-    "env": "${INPUT_ENV}"
+    "env": "${INPUT_ENV}",
+    "allow_chained_deployments": "${INPUT_ALLOW_CHAINED_DEPLOYMENTS}"
   }
 }
 EOF
