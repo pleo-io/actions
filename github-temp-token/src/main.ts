50571a988998e503c5e42637a0ac1fc7b5bba7f4
Maciek Pekala <m@peka.la>
2020-12-14 11:43:43 +0100

    Use the new auth app lib in temp token action

--- github-temp-token/src/main.ts
+++ github-temp-token/src/main.ts
@@ -1,16 +1,19 @@
 import * as core from "@actions/core";
-import { App } from "@octokit/app";
+import { createAppAuth } from "@octokit/auth-app";
 
 async function run() {
   const githubAppID = +core.getInput("github_app_id");
   const githubAppInstallationID = +core.getInput("github_app_installation_id");
   const githubAppPrivateKey = core.getInput("github_app_private_key");
 
-  const app = new App({ id: githubAppID, privateKey: githubAppPrivateKey });
-  const token = await app.getInstallationAccessToken({
-    installationId: githubAppInstallationID
+  const auth = createAppAuth({
+    appId: githubAppID,
+    privateKey: githubAppPrivateKey,
+    installationId: githubAppInstallationID,
   });
-  core.setOutput('github_temp_token', token)
+
+  const { token } = await auth({ type: "installation" });
+  core.setOutput("github_temp_token", token);
 }
 
-run().catch(error => core.setFailed(error.message));
+run().catch((error) => core.setFailed(error.message));
